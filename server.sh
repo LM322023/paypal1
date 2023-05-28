b06c6ad00d9954921c1af39c039a5e6083a6e66c
tahmichael25 <tahmichael_5@yahoo.co.uk>
2023-05-28 21:28:12 +0000

    adding server file1

--- server.sh
+++ server.sh
@@ -1,37 +1,37 @@
-root:x:0:0:root:/root:/bin/bash
-bin:x:1:1:bin:/bin:/sbin/nologin
-daemon:x:2:2:daemon:/sbin:/sbin/nologin
-adm:x:3:4:adm:/var/adm:/sbin/nologin
-lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin
-sync:x:5:0:sync:/sbin:/bin/sync
-shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
-halt:x:7:0:halt:/sbin:/sbin/halt
-mail:x:8:12:mail:/var/spool/mail:/sbin/nologin
-operator:x:11:0:operator:/root:/sbin/nologin
-games:x:12:100:games:/usr/games:/sbin/nologin
-ftp:x:14:50:FTP User:/var/ftp:/sbin/nologin
-nobody:x:65534:65534:Kernel Overflow User:/:/sbin/nologin
-dbus:x:81:81:System message bus:/:/sbin/nologin
-systemd-network:x:192:192:systemd Network Management:/:/usr/sbin/nologin
-systemd-oom:x:999:999:systemd Userspace OOM Killer:/:/usr/sbin/nologin
-systemd-resolve:x:193:193:systemd Resolver:/:/usr/sbin/nologin
-sshd:x:74:74:Privilege-separated SSH:/usr/share/empty.sshd:/sbin/nologin
-rpc:x:32:32:Rpcbind Daemon:/var/lib/rpcbind:/sbin/nologin
-libstoragemgmt:x:997:997:daemon account for libstoragemgmt:/:/usr/sbin/nologin
-systemd-coredump:x:996:996:systemd Core Dumper:/:/usr/sbin/nologin
-systemd-timesync:x:995:995:systemd Time Synchronization:/:/usr/sbin/nologin
-ec2-instance-connect:x:994:994::/home/ec2-instance-connect:/sbin/nologin
-rpcuser:x:29:29:RPC Service User:/var/lib/nfs:/sbin/nologin
-chrony:x:993:993:chrony system user:/var/lib/chrony:/sbin/nologin
-tcpdump:x:72:72::/:/sbin/nologin
-ec2-user:x:1000:1000:EC2 Default User:/home/ec2-user:/bin/bash
-oke:x:1001:1001::/home/oke:/bin/bash
-apache:x:48:48:Apache:/usr/share/httpd:/sbin/nologin
-tom:x:1002:1002::/home/tom:/bin/bash
-nginx:x:992:992:Nginx web server:/var/lib/nginx:/sbin/nologin
-miketah:x:1003:1003::/home/miketah:/bin/bash
-smith:x:1004:1004::/home/smith:/bin/bash
-chidi:x:1005:1005::/home/chidi:/bin/bash
-oboso:x:1006:1006::/home/oboso:/bin/bash
-obim:x:1007:1007::/home/obim:/bin/bash
-jake:x:1008:1008::/home/jake:/bin/bash
+root:*LOCK*:14600::::::
+bin:*:19387:0:99999:7:::
+daemon:*:19387:0:99999:7:::
+adm:*:19387:0:99999:7:::
+lp:*:19387:0:99999:7:::
+sync:*:19387:0:99999:7:::
+shutdown:*:19387:0:99999:7:::
+halt:*:19387:0:99999:7:::
+mail:*:19387:0:99999:7:::
+operator:*:19387:0:99999:7:::
+games:*:19387:0:99999:7:::
+ftp:*:19387:0:99999:7:::
+nobody:*:19387:0:99999:7:::
+dbus:!!:19478::::::
+systemd-network:!*:19478::::::
+systemd-oom:!*:19478::::::
+systemd-resolve:!*:19478::::::
+sshd:!!:19478::::::
+rpc:!!:19478:0:99999:7:::
+libstoragemgmt:!*:19478::::::
+systemd-coredump:!*:19478::::::
+systemd-timesync:!*:19478::::::
+ec2-instance-connect:!!:19478::::::
+rpcuser:!!:19478::::::
+chrony:!!:19478::::::
+tcpdump:!!:19478::::::
+ec2-user:!!:19490:0:99999:7:::
+oke:$6$vR/s6KZ9jYwjq.u4$nP47Vs2dPGd7xrmxx8ELSCvqQqRPHfmouEAQiT2YOL9HT/AV2l3IR/tTDMXEGJsDQzQ8t3jl/qKcDJnDqMAy8/:19490:0:99999:7:::
+apache:!!:19490::::::
+tom:$6$hlRa.gdJvNJbk7xC$RqkZ5zMULbbKzLgAzbcr6LJBwT27.IzLB3QWh/b43UB/qeEKRN8pYXcVSGHiHBWAPUPsp21YXb57CXLZoO.hD/:19491:0:99999:7:::
+nginx:!!:19491::::::
+miketah:$6$HuOjQPqbMk3FCdCb$99fnr1HaQmi0akNnI8QL.Yk336uWnPlSnv1ETfcNn/bW1V8Hvi96a3veDO8PuMLcvjrUpzdagrFwSv2INwdNy/:19491:0:99999:7:::
+smith:$6$ZCWyHZwmcATWauWS$uuO1/29De8wfmELTWWB.5AnmSk/DXCfBsJenBiTJRcs6YxsFTnNj99E/uZcFVGhDwjgCdvlnCpcOCxbZ52HtI1:19495:0:99999:7:::
+chidi:!!:19497:0:99999:7:::
+oboso:!!:19497:0:99999:7:::
+obim:!!:19497:0:99999:7:::
+jake:!!:19497:0:99999:7:::
